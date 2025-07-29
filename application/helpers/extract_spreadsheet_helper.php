90614e07384f5f9a811a9f154b2c3ca070eb9aed
oki <dwiputeraan@gmail.com>
2025-07-29 14:10:24 +0700

    fix extract_spreadsheet_helper

--- application/helpers/extract_spreadsheet_helper.php
+++ application/helpers/extract_spreadsheet_helper.php
@@ -4,17 +4,39 @@ use PhpOffice\PhpSpreadsheet\IOFactory;
 
 function extract_spreadsheet($file_path, $with_sheet_names = false)
 {
-    $spreadsheet = IOFactory::load($file_path);
-    // $sheet = $spreadsheet->getActiveSheet();
+    $reader = IOFactory::createReaderForFile($file_path);
+    $reader->setReadDataOnly(false); // Biar bisa cek formula
+    $spreadsheet = $reader->load($file_path);
+
     $sheets = $spreadsheet->getSheetNames();
-    foreach ($sheets as $i_sheet => $sheet_i) {
-        $rows = $spreadsheet->getSheet($i_sheet);;
-        $rows = $rows->toArray();
+    $extracted_sheet = [];
+
+    foreach ($sheets as $i_sheet => $sheet_name) {
+        $sheet = $spreadsheet->getSheet($i_sheet);
+        $rows = [];
+
+        foreach ($sheet->getRowIterator() as $row) {
+            $rowData = [];
+            $cellIterator = $row->getCellIterator();
+            $cellIterator->setIterateOnlyExistingCells(false);
+
+            foreach ($cellIterator as $cell) {
+                if ($cell->isFormula()) {
+                    $rowData[] = $cell->getOldCalculatedValue(); // Nilai cached
+                } else {
+                    $rowData[] = $cell->getValue();
+                }
+            }
+            $rows[] = $rowData;
+        }
+
         if ($with_sheet_names === false) {
             $extracted_sheet[$i_sheet] = $rows;
         } else {
-            $extracted_sheet[$i_sheet]['name'] = $sheet_i;
-            $extracted_sheet[$i_sheet]['rows'] = $rows;
+            $extracted_sheet[$i_sheet] = [
+                'name' => $sheet_name,
+                'rows' => $rows
+            ];
         }
     }
 

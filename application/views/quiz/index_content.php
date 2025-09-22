e4e13d1cf5b1d1347a4ec66522f47283407a906f
oki <dwiputeraan@gmail.com>
2025-09-22 17:55:06 +0700

    fix

--- application/views/quiz/index_content.php
+++ application/views/quiz/index_content.php
@@ -4,10 +4,6 @@
         <div class="row">
             <div class="col-12 d-flex justify-content-between align-items-center mb-3">
                 <h3 class="mb-0"><i class="fas fa-gamepad mr-1"></i> Join Quiz</h3>
-                <!-- opsional: link ke admin dashboard -->
-                <a class="btn btn-outline-secondary" href="<?= site_url('quiz_admin'); ?>">
-                    <i class="fas fa-sliders-h mr-1"></i> Admin
-                </a>
             </div>
         </div>
 
